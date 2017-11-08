b0d692aa3a585e005a0e7b53b44efa72f7ac163f
杨汇琛 <32983694+Huic-Yang@users.noreply.github.com>
2017-11-09 00:39:17 +0800

    add comments

--- Exercise 3 Numbers and Math/ex3.py
+++ Exercise 3 Numbers and Math/ex3.py
@@ -1,23 +1,36 @@
+# count chickens
 print("I will now count my chickens:")
 
+# count the number of hens.
+# The result is a float because it has a div operation.
 print("hens", 25 + 30 / 6)
+# count the number of roosters.
+# It cal 25 * 3 % 4 from left to right.
 print("Roosters", 100 - 25 * 3 % 4)
 
+# Counting the eggs.
 print("Now I will count the eggs:")
 
+# Result is a float number.
 print(3 + 2 + 1 - 5 + 4%2 - 1/4 + 6)
 
+# Test 3 + 2 < 5 - 7
 print("Is it true that 3 + 2 < 5 - 7?")
 
+# Result is a boolean value.
 print(3 + 2 < 5 - 7)
 
+# Print the result of two expression.
 print("What is 3 + 2?", 3 + 2)
 print("What is 5 - 7?", 5 - 7)
 
+# Yes, it is false obiviously.
 print("Oh, that is why it is False.")
 
+# Learn more things.
 print("How about some more.")
 
+# Use different operator.
 print("Is it greater?", 5 > -2)
 print("Is it greater or equal?", 5 >= -2)
 print("Is it less or equal?", 5 <= -2)
