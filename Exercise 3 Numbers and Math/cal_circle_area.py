853043f0c61dd70a22b40cd094c0d1b888102d38
杨汇琛 <32983694+Huic-Yang@users.noreply.github.com>
2017-11-09 00:43:01 +0800

    add cal_circle_area.py

---
+++ Exercise 3 Numbers and Math/cal_circle_area.py
@@ -0,0 +1,10 @@
+pi = 3.1415926
+
+# enter the radius of circle
+radius = input("Enter the radius: ")
+
+# calculate the area
+area = pi*radius*radius
+
+# output the Result
+print("The area of this circle is: ", area)
