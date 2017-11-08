ae298221fc1b5e78c156bb62bead7a9f53332ad6
杨汇琛 <32983694+Huic-Yang@users.noreply.github.com>
2017-11-09 00:45:02 +0800

    correct some error

--- Exercise 3 Numbers and Math/cal_circle_area.py
+++ Exercise 3 Numbers and Math/cal_circle_area.py
@@ -1,7 +1,8 @@
 pi = 3.1415926
 
 # enter the radius of circle
-radius = input("Enter the radius: ")
+# input will be a string, should convert to a number.
+radius = float(input("Enter the radius: "))
 
 # calculate the area
 area = pi*radius*radius
